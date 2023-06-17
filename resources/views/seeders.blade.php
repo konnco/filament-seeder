7ff16a4a4e0276c651d3fe07910ddc5a36f84b91
hafizabd <abduromanov11@gmail.com>
2023-06-17 16:19:42 +0700

    feat: update dependencies with laravel 10, fix view in dark mode

--- resources/views/seeders.blade.php
+++ resources/views/seeders.blade.php
@@ -1,5 +1,5 @@
 <x-filament::page>
-    <form wire:submit.prevent="submit" class="filament-forms-card-component p-6 bg-white rounded-xl border border-gray-300">
+    <form wire:submit.prevent="submit" class="filament-forms-card-component p-6 bg-white rounded-xl border border-gray-300 dark:border-gray-700 dark:bg-gray-800">
         {{ $this->form }}
 
         <x-forms::button wire:loading.attr="disabled" type="submit" class="mt-4 w-full">
